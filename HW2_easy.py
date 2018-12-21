809dae5ef1469c5ad0aca846c67bcab5f3800f4c
StoneGolem1 <oleggin2006@yandex.ru>
2018-12-21 16:03:19 +0300

    Домашнее задание 2

---
+++ HW2_easy.py
@@ -0,0 +1,39 @@
+# Задача 1
+fruit = ['арбуз', 'дыня', 'виноград', 'гранат', 'персик']
+i=0
+while i!= len(fruit):
+    i+=1
+    print(f'{i}. {fruit[i-1]:>10}')
+
+# Задача 2
+list1 = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 'no', 'yes', 'no']
+list2 = [1, 2, 3, 'no']
+print(list1)
+print(list2)
+i=0
+while i != len(list2):
+    j = 0
+    while j != len(list1):
+       if list2[i] == list1[j]:
+           list1.remove(list2[i])
+       else:
+           j += 1
+    i += 1
+print(list1)
+
+# Задача 3
+import random
+n = random.randrange(5, 10, 1)
+list1 = []
+i = 0
+while i != n:
+   elem = random.randrange(1, 100, 1)
+   list1.append(elem)
+   i += 1
+i = 0
+list2 = []
+while i != len(list1):
+    if list
+    i += 1
+print(list1)
+print(list2)
---
+++ HW3_easy.py
@@ -0,0 +1,11 @@
+print('Задача 1')
+def roundto(x, y):
+    d = 10**(-y)
+    a = x%d
+    if a == 0
+
+    return a
+
+
+print('Задача 2')
+
